5de067896503ec6e9ef45b33dfd104f96d0a49ae
douglasryu <douglasryu@hotmail.com>
2021-04-01 12:14:28 -0500

    Update heroku

--- config/database.js
+++ config/database.js
@@ -14,10 +14,22 @@ module.exports = {
         host,
         dialect: 'postgres',
         seederStorage: 'sequelize',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        }
     },
     production: {
         dialect: 'postgres',
         seederStorage: 'sequelize',
-        use_env_variable: 'DATABASE_URL'
+        use_env_variable: 'DATABASE_URL',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        }
     }
 };
